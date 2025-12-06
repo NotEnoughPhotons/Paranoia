451ad80e68a0b944c490db029df8a38fade8163a
adamdev <adamdevofficial@protonmail.com>
2025-12-06 02:02:57 -0500

    Events: New check to see if it can start at a time

--- Scripts/Events/ParanoiaEvent.cs
+++ Scripts/Events/ParanoiaEvent.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace NEP.Paranoia.Events
 {
@@ -7,11 +8,30 @@ namespace NEP.Paranoia.Events
         public bool Started => m_started;
         
         private bool m_started;
+        
         private float m_insanityLevel;
 
+        private float m_timer = 0f;
+        private float m_nextTimeToRun = 0f;
+        
+        public bool CanStart()
+        {
+            if (m_started)
+                return false;
+            
+            m_timer += Time.deltaTime;
+
+            if (m_timer < m_nextTimeToRun)
+                return false;
+            
+            m_timer = 0f;
+            return true;
+        }
+        
         public virtual void Start()
         {
             m_started = true;
+            m_nextTimeToRun = Random.Range(0f, 15f);
         }
 
         public virtual void Update()
