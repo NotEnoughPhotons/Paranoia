b1b7125c29a22027be15bc2d1df3e72660031467
adamdev <adamdevofficial@protonmail.com>
2025-12-06 04:47:42 -0500

    Entity: Fix a bug with Ambient not resetting after sound finished

--- Scripts/Entities/Ambient.cs
+++ Scripts/Entities/Ambient.cs
@@ -2,20 +2,49 @@
 using NEP.Paranoia.Audio;
 using UnityEngine;
 
-using NEP.Paranoia.Managers;
+using Random = UnityEngine.Random;
 
 namespace NEP.Paranoia.Entities
 {
     [RegisterTypeInIl2Cpp]
     public class Ambient(IntPtr ptr) : Entity(ptr)
     {
+        private float m_audioTimer = 0f;
+        private float m_audioDuration = 0f;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            SetInsanity(1.0f);
+        }
+
         public override void EntityStart()
         {
             base.EntityStart();
-            
+
+            Appear();
             UseAudio();
             SetSpatial(0f);
-            Emit(AudioBank.Ambience);
+
+            AudioClip clip = AudioBank.Ambience[Random.Range(0, AudioBank.Ambience.Length - 1)];
+
+            m_audioDuration = clip.length;
+
+            Emit(clip);
+        }
+
+        protected override void EntityUpdate()
+        {
+            m_audioTimer += Time.unscaledDeltaTime;
+
+            if (m_audioTimer >= m_audioDuration)
+                Disappear();
+        }
+
+        public override void EntityStop()
+        {
+            m_audioTimer = 0f;
+            m_audioDuration = 0f;
         }
     }
 }
