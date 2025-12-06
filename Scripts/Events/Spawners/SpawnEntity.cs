9c1ca33013d8276af9fbdea9060c6a18f8fc965e
adamdev <adamdevofficial@protonmail.com>
2025-12-06 02:01:17 -0500

    Events: SpawnEntity now spawns entities from a list

--- Scripts/Events/Spawners/SpawnEntity.cs
+++ Scripts/Events/Spawners/SpawnEntity.cs
@@ -1,7 +1,7 @@
 ﻿using BoneLib;
 using Il2CppSLZ.Marrow.Warehouse;
 using NEP.Paranoia.Entities;
-
+using NEP.Paranoia.Managers;
 using UnityEngine;
 
 using Random = UnityEngine.Random;
@@ -12,28 +12,12 @@ namespace NEP.Paranoia.Events.Spawners
     {
         public override void Start()
         {
-            AssetWarehouse.Instance.TryGetPallet(new Barcode("NEP.Paranoia"), out Pallet pallet);
+            base.Start();
 
-            if (pallet)
-            {
-                List<SpawnableCrateReference> entities = new List<SpawnableCrateReference>();
-
-                foreach (var crate in pallet.Crates)
-                {
-                    foreach (var tag in crate.Tags)
-                    {
-                        if (tag == "Entity")
-                            entities.Add(new SpawnableCrateReference() { Barcode = crate.Barcode });
-                    }
-                }
-                
-                var randEnt = entities[Random.Range(0, entities.Count)];
-                HelperMethods.SpawnCrate(randEnt, Vector3.zero, Quaternion.identity, Vector3.one, false, (obj) =>
-                {
-                    Entity entity = obj.GetComponent<Entity>();
-                    entity.EntityStart();
-                });
-            }
+            Entity entity = ParanoiaDirector.Entities[Random.Range(0, ParanoiaDirector.Entities.Count - 1)];
+            ParanoiaDirector.Spawn(entity);
+            
+            base.Stop();
         }
     }
 }
